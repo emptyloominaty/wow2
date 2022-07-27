fd2670511223a9c5055b61b82f7c43c126b750d7
emptyloominaty <cipuksiblol@gmail.com>
2022-07-27 22:01:05 +0200

    260 (EnhancementShaman4)

--- js/class/abilities/shaman/_enhancement_talents.js
+++ js/class/abilities/shaman/_enhancement_talents.js
@@ -16,24 +16,24 @@ let _enhancement_talents = function(caster) {
     caster.abilities["Static Charge"] = new StaticCharge()
 
     //4
-    //caster.abilities["Elemental Assault"] = new ElementalAssault()
-    //caster.abilities["Hailstorm"] = new Hailstorm()
-    //caster.abilities["Fire Nova"] = new FireNova()
+    caster.abilities["Elemental Assault"] = new ElementalAssault()
+    caster.abilities["Hailstorm"] = new Hailstorm()
+    caster.abilities["Fire Nova"] = new FireNova()
 
     //5
     caster.abilities["Nature's Guardian"] = new NaturesGuardian()
-    //caster.abilities["Feral Lunge"] = new FeralLunge()
+    caster.abilities["Feral Lunge"] = new FeralLunge()
     caster.abilities["Wind Rush Totem"] = new WindRushTotem()
 
     //6
-    //caster.abilities["Crashing Storm"] = new CrashingStorm()
-    //caster.abilities["Stormkeeper"] = new Stormkeeper()
-    //caster.abilities["Sundering"] = new Sundering()
+    caster.abilities["Crashing Storm"] = new CrashingStorm()
+    caster.abilities["Stormkeeper"] = new StormkeeperEnh()
+    caster.abilities["Sundering"] = new Sundering()
 
     //7
-    //caster.abilities["Elemental Spirits"] = new ElementalSpirits()
-    //caster.abilities["Earthen Spike"] = new EarthenSpike()
-    //caster.abilities["Ascendance"] = new Ascendance()
+    caster.abilities["Elemental Spirits"] = new ElementalSpirits()
+    caster.abilities["Earthen Spike"] = new EarthenSpike()
+    caster.abilities["Ascendance"] = new Ascendance()
 
 
     caster.talents = [["Lashing Flames","Forceful Winds","Elemental Blast"],
@@ -105,7 +105,6 @@ class HotHand extends Ability {
 class IceStrike extends Ability {
     constructor() {
         super("Ice Strike", 3.3, 1.5, 0, 15, false, false, false, "frost", 5, 1)
-        this.passive = true
         this.talent = true
         this.duration = 6
     }
@@ -117,18 +116,187 @@ class IceStrike extends Ability {
     }
 
 }
-//------------------------------------------------------------------------------------------------ROW3
-//------------------------------------------------
-//------------------------------------------------
 //------------------------------------------------------------------------------------------------ROW4
+class ElementalAssault extends Ability {
+    constructor() {
+        super("Elemental Assault", 0, 0, 0, 0, false, false, false, "physical", 5, 1)
+        this.passive = true
+        this.talent = true
+        this.talentSelect = true
+    }
+
+    getTooltip() {
+        return "Stormstrike damage is increased by 15%, and Stormstrike now generates 1 stack of Maelstrom Weapon."
+    }
+
+
+}
 //------------------------------------------------
+class Hailstorm extends Ability {
+    constructor() {
+        super("Hailstorm", 0, 0, 0, 0, false, false, false, "physical", 5, 1)
+        this.passive = true
+        this.talent = true
+    }
+
+    getTooltip() {//TODO:
+        return "//NOT IMPLEMENTED//Each stack of Maelstrom Weapon consumed increases the damage of your next Frost Shock by 15%, and causes your next Frost Shock to hit 1 additional target per Maelstrom Weapon stack consumed."
+    }
+
+
+}
 //------------------------------------------------
+class FireNova extends Ability {
+    constructor() {
+        super("Fire Nova", 1, 1.5, 0, 15, false, false, false, "fire", 5, 1)
+        this.talent = true
+    }
+
+    getTooltip() { //TODO
+        return "//NOT IMPLEMENTED//Erupt a burst of fiery damage from all targets affected by your Flame Shock, dealing (42% of Attack power) Fire damage to up to 6 targets within 8 yds of your Flame Shock targets."
+    }
+}
 //------------------------------------------------------------------------------------------------ROW5
 //------------------------------------------------
+class FeralLunge extends Ability {
+    constructor() {
+        super("Feral Lunge", 0, 0.5, 0, 30, false, false, false, "physical", 25, 1)
+        this.talent = true
+        this.minRange = 8
+    }
+
+    getTooltip() { //TODO
+        return "//NOT IMPLEMENTED//Lunge at your enemy as a ghostly wolf, biting them to deal (9% of Attack power) Physical damage."
+
+    }
+}
 //------------------------------------------------
 //------------------------------------------------------------------------------------------------ROW6
+class CrashingStorm extends Ability {
+    constructor() {
+        super("Crashing Storm", 0, 0, 0, 0, false, false, false, "physical", 5, 1)
+        this.passive = true
+        this.talent = true
+    }
+
+    getTooltip() {//TODO:
+        return "//NOT IMPLEMENTED//Crash Lightning also electrifies the ground, leaving an electrical field behind which damages enemies within it for [7 * (2.688% of Attack power)] Nature damage over 6 sec."
+    }
+
+}
 //------------------------------------------------
+class StormkeeperEnh extends Ability {
+    constructor() {
+        super("Stormkeeper", 0, 1.5, 1.5, 60, false, true, false, "physical", 5, 1)
+             this.talent = true
+    }
+
+    getTooltip() {//TODO:
+        return "//NOT IMPLEMENTED//Charge yourself with lightning, causing your next 2 Chain Lightnings to deal 300% more damage and be instant cast."
+    }
+
+    getBuffTooltip(caster, target, buff) {
+        return "Your next Chain Lightning will deal 300% increased damage and be instant cast."
+    }
+
+}
 //------------------------------------------------
+class Sundering extends Ability {
+    constructor() {
+        super("Sundering", 6, 1.5, 0, 40, false, false, false, "fire", 5, 1)
+        this.talent = true
+        this.talentSelect = true
+        this.spellPower = 1.4
+        this.duration = 2
+        this.effect = [{name:"incapacitate"}]
+    }
+
+    getTooltip() {
+        return "Shatters a line of earth in front of you with your main hand weapon, causing "+spellPowerToNumber(this.spellPower)+" Flamestrike damage and Incapacitating any enemy hit for 2 sec."
+    }
+    startCast(caster) {
+        if (this.checkStart(caster)) {
+            let dir = caster.direction
+            let targets = enemies
+            for (let i = 0; i<targets.length ;i++) {
+                if (!targets[i].isDead && this.checkDistance(caster, targets[i],undefined,true)) {
+                    let dirToTarget = getDirection(caster,targets[i])
+                    if (directionHit(dir,dirToTarget,75)) {
+                        doDamage(caster, targets[i], this)
+                        applyDebuff(caster, targets[i], this)
+                    }
+                }
+            }
+
+            if (caster.isChanneling) {
+                caster.isChanneling = false
+            }
+            this.setCd()
+            caster.useEnergy(this.cost,this.secCost)
+            this.setGcd(caster)
+            return true
+        } else if (this.canSpellQueue(caster)) {
+            spellQueue.add(this,caster.gcd)
+        }
+        return false
+    }
+
+}
 //------------------------------------------------------------------------------------------------ROW7
+class ElementalSpirits extends Ability {
+    constructor() {
+        super("Elemental Spirits", 0, 0, 0, 0, false, false, false, "physical", 5, 1)
+        this.passive = true
+        this.talent = true
+        this.talentSelect = true
+    }
+
+    //TODO:Molten Weapon:: FlametongueWeapon, LavaLash, Sundering, ElementalBlast, FlameShock, FlameShockDirect, FireNova +20% dmg
+    //TODO:Crackling Surge:: StormstrikeMain, StormstrikeOff, Windfury, WindstrikeMain, WindstrikeOff +35% dmg
+    //TODO:Frost:: AutoAttackMainEnhance, AutoAttackOffEnhance +21% dmg
+
+    getTooltip() { //TODO:causes your Feral Spirits to be imbued with Fire, Frost, or Lightning, enhancing your abilities."
+        return "Reduces the cooldown of Feral Spirit by 30 sec and causes your Feral Spirits to be imbued with Fire, Frost, or Lightning, enhancing your abilities."
+    }
+
+    setTalent(caster) {
+        caster.abilities["Feral Spirit"].cd -= 30
+    }
+
+    unsetTalent(caster) {
+        caster.abilities["Feral Spirit"].cd += 30
+    }
+
+}
 //------------------------------------------------
+class EarthenSpike extends Ability {
+    constructor() {
+        super("Earthen Spike", 3.75, 1.5, 0, 20, false, false, false, "physical", 10, 1)
+        this.passive = true
+        this.talent = true
+    }
+
+    getTooltip() {//TODO:
+        return "//NOT IMPLEMENTED//Summons an Earthen Spike under an enemy, dealing (108% of Attack power) Physical damage and increasing Physical and Nature damage you deal to the target by 20% for 10 sec."
+    }
+
+}
 //------------------------------------------------
+class Ascendance extends Ability {
+    constructor() {
+        super("Ascendance", 0, 1.5, 0, 180, false, false, false, "physical", 10, 1)
+        this.passive = true
+        this.talent = true
+    }
+
+    getTooltip() {//TODO:
+        return "//NOT IMPLEMENTED//Transform into an Air Ascendant for 15 sec, immediately dealing (250% of Attack power) Nature damage to any enemy within 8 yds," +
+            " reducing the cooldown and cost of Stormstrike by 66%, and transforming your auto attack and Stormstrike into Wind attacks which bypass armor and have a 30 yd range."
+    }
+
+    //Windstrike
+    //9sec cd
+    //Hurl a staggering blast of wind at an enemy, dealing a total of [(107.4% of Attack power)% + (107.4% of Attack power)%] Physical damage, bypassing armor.
+    //30yards
+    //
+}
